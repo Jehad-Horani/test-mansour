93abe0dfdae117ef1728a67501ae3d22346580ce
Mansour Dhair <75341740+mansour-766@users.noreply.github.com>
2025-09-19 18:36:43 +0300

    Updated use-user.ts

--- app/hooks/use-user.ts
+++ app/hooks/use-user.ts
@@ -1,6 +1,1 @@
-@@ .. @@
-       },
-     }
-     setUser(newUser)
-     return newUser
-+  }
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
