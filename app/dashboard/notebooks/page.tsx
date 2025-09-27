42d097f7ca9ac51869153b52d28320e3651d14b9
Jehad-Horani <jehadhorani18@gmail.com>
2025-09-27 18:10:52 +0300

    1

--- app/dashboard/notebooks/page.tsx
+++ app/dashboard/notebooks/page.tsx
@@ -340,7 +340,7 @@ export default function NotebooksPage() {
           </div>
         )}
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
           {/* My Lectures */}
           <RetroWindow title="محاضراتي">
             <div className="p-4">
